b5e59b00e5b0410e1fbd2ac2de35047bee04b635
Tobias Langendorf <junglerobba@jngl.one>
2024-01-13 14:02:54 +0100

    add flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems =
+        [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" "aarch64-linux" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          cargoToml = pkgs.lib.importTOML "${self}/Cargo.toml";
+          obs-livesplit-remote = with pkgs;
+            rustPlatform.buildRustPackage {
+              inherit (cargoToml.package) name version;
+              cargoLock.lockFile = ./Cargo.lock;
+              src = self;
+              nativeBuildInputs = [ pkg-config ];
+              buildInputs = [ openssl ];
+            };
+        in {
+          packages = { default = obs-livesplit-remote; };
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs;
+              with pkgs.rustPlatform;
+              [ cargo rustc rustfmt rust-analyzer openssl pkg-config ]
+              ++ lib.optionals stdenv.isDarwin [ darwin.libiconv ];
+          };
+        };
+    };
+}
